e4f9b10e943d87688d8b6b312346375af353788a
Alamgir Ahamed <alamgir72435@gmail.com>
2023-10-22 18:00:37 +0600

    module federation implement

---
+++ cart/src/bootstrap.js
@@ -0,0 +1,6 @@
+import faker from 'faker';
+
+const cartText=     `<div>You have ${faker.random.number()} items in cart</div>`;
+
+document.querySelector('#cart-dev').innerHTML = cartText;
+// test
---
+++ cart/webpack.config.js
@@ -0,0 +1,25 @@
+
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+    mode:"development",
+    devServer:{
+        port: 8082
+    },
+    plugins:[
+        new ModuleFederationPlugin({
+            name:"cart",
+            filename:"remoteEntry.js",
+            exposes:{
+                './CartShow':"./src/index",
+                // other exposes
+            },
+            shared:['faker']
+        }),
+        new HtmlWebPackPlugin({
+            template:"./public/index.html"
+        })
+    ]
+}
